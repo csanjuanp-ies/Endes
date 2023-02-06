05f4b64d8e9cdb7f1e340f67a7606614f042dc40
csanjuanp-ies <csanjuap.ies@gmail.com>
2023-02-06 09:03:09 +0100

    Commit

--- ut_4/004_5/dataclass/Prestamo.py
+++ ut_4/004_5/dataclass/Prestamo.py
@@ -1,14 +1,16 @@
 from dataclass.Libro import Libro
 from dataclass.Usuario import Usuario
-import datetime
+from datetime import datetime, timedelta
+
 class Prestamo:
-    DIAS_DE_PRESTAMO_POR_DEFECTO = datetime.timedelta(days=5)
-    def __init__(self, asunto):
-        self._libro = None
-        self._usuario = None
-        self.asunto = asunto
-        self.fecha_de_prestamo = datetime.datetime.now()
-        self.fecha_de_devolucion = self.fecha_de_prestamo + Prestamo.DIAS_DE_PRESTAMO_POR_DEFECTO
+    DIAS_DE_PRESTAMO_POR_DEFECTO = timedelta(days=5)
+    def __init__(self, asunto:str):
+        self._libro:Libro|None = None
+        self._usuario:Usuario|None = None
+        self.asunto:str = asunto
+        self.fecha_de_prestamo:datetime = datetime.now()
+        self.fecha_prevista_de_devolucion:datetime = self.fecha_de_prestamo + Prestamo.DIAS_DE_PRESTAMO_POR_DEFECTO
+        self.fecha_de_devolucion: datetime|None = None
 
     def __eq__(self, other:"Prestamo"):
         return self._libro == other._libro and \
@@ -27,3 +29,10 @@ class Prestamo:
     @usuario.setter
     def usuario(self, valor: Usuario):
         self._usuario = valor
+    @property
+    def devuelto_tarde(self):
+        if self.fecha_de_devolucion is not None:
+            return self.fecha_prevista_de_devolucion <= self.fecha_de_devolucion
+        return None
+    def cerrar_prestamo(self):
+        self.fecha_de_devolucion = datetime.now()
